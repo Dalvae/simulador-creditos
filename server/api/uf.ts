91641c5a29a8abf32808a0429a1fb0d7da958da6
Dalvae <diego.alvarez.e@ug.uchile.cl>
2024-05-09 09:49:55 -0400

    fixed uf server side

--- server/api/uf.ts
+++ server/api/uf.ts
@@ -1,20 +1,42 @@
 import axios from "axios";
 
-export default defineEventHandler(async (event) => {
+let cachedUfValue: number | null = null;
+let cachedUpdatedAt: string | null = null;
+
+const getUfValue = async () => {
   try {
     const response = await axios.get("https://mindicador.cl/api");
     const ufValue = response.data.uf.valor;
 
     if (!ufValue) {
-      throw new Error("No se pudo obtener el valor de la UF");
+      throw new Error("no se pudo obtener el valor de la UF");
     }
-    return {
-      ufValue,
-      updatedAt: new Date().toISOString(),
-    };
+
+    cachedUfValue = ufValue;
+    cachedUpdatedAt = new Date().toISOString();
+
+    console.log("UF del dia", ufValue);
   } catch (error: unknown) {
-    return {
-      error,
-    };
+    console.error("Error:", error);
   }
+};
+
+getUfValue();
+
+// cada 24 horas
+setInterval(() => {
+  getUfValue();
+}, 24 * 60 * 60 * 1000);
+
+export default defineEventHandler((event) => {
+  if (!cachedUfValue || !cachedUpdatedAt) {
+    throw createError({
+      statusCode: 500,
+    });
+  }
+
+  return {
+    ufValue: cachedUfValue,
+    updatedAt: cachedUpdatedAt,
+  };
 });
